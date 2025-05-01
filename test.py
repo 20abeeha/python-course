4df9c498d19cae69e164b2ebdb6ffcc963626046
20abeeha <javeriaraza54@gmail.com>
2025-05-01 17:39:53 +0100

    Add files via upload

---
+++ test.py
@@ -0,0 +1,13 @@
+print('Welcome to my chatbot univers')
+name=input('Hey there may i know your name?')
+print(f' Hi {name} how can i help you in goegrapy today?')
+doubt=input()
+if(doubt=='coasts'):
+    print('A coast is the land next to the sea or the boundary between land and the ocean or a lake. Coasts are influenced by ' 
+    'the topography of the surrounding landscape and water-induced erosion, such as waves. They are productive and diverse' 
+    ' landscapes that link land and ocean, supporting critical ecosystems,' 
+    ' local communities, and economies')
+
+else:
+    print('Whould you like ask me about coast it is so interesting!')
+    
